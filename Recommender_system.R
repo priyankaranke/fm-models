169ebb35c73754fc784fea9b0ee50709c520dec6
priyankaranke <aranke.priyank99@gmail.com>
2016-08-18 10:25:16 +0530

    Add files via upload

---
+++ Recommender_system.R
@@ -0,0 +1,24 @@
+library("recommenderlab")
+library(Matrix)
+
+#Setting row names to be cities and deleting columns
+df <- read.csv("Capital First - Capital first sheet.csv")
+df$State <- NULL
+df$Area <- NULL
+row.names(df) <- df$City
+df$City <- NULL
+
+#transposing so that it'll recommend me cities and not franchises
+df_t <- t(df)
+transposeddf <- as.data.frame(df_t)
+
+df1<-as.matrix(transposeddf) #It doesn't work without this step
+df1<-sapply(data.frame(df1),as.numeric) # Convert to numeric (by arbitrarily mapping characters to numbers.)
+
+recobuilder <- as(df1, "realRatingMatrix")
+rec <- Recommender(recobuilder, method = "UBCF") #Building a recommender model with User-based CF
+
+# ---Building top-N predictions using recommender model---
+pre <- predict(rec, recobuilder[26, ], n = 10)
+as(pre, "list")
+
